5fc48b01dca3d5431330d210aacac0f03cfb2fb8
Parth Maru <theparthmaru@gmail.com>
2024-01-26 13:16:48 +0530

    Added 217. Contains_duplicate.js

---
+++ leetcode/217. Contains_duplicate.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/**
+ * Approach 1: Creating a new Set from nums
+ * Time complexity: O(n) (Because creating set from nums array of size n)
+ * Space complexity: O(n)
+ */
+
+function solution(nums) {
+	let mySet = new Set(nums);
+	if (nums.length === mySet.size) return false;
+	return true;
+}
+
