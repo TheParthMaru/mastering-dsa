468e339efd64e2e606519701e80e2a613cfb5073
Parth Maru <theparthmaru@gmail.com>
2024-01-27 09:51:23 +0530

    Added new approach with better space complexity

--- leetcode/58. lenght_of_last_word.js
+++ leetcode/58. lenght_of_last_word.js
@@ -13,3 +13,32 @@ function solution(s) {
 	let sArr = s.split(" ");
 	return sArr[sArr.length - 1].length;
 }
+
+/**
+ * Approach 2: Improving the space complexity but not creating additional array
+ *
+ * Time complexity: O(N)
+ * Space complexity: O(1)
+ */
+
+var lengthOfLastWord = function (s) {
+	let firstChar = false;
+	let lenLastWord = 0;
+
+	// Looping over the string from the end
+	for (let i = s.length - 1; i >= 0; i--) {
+		// Checking if the current character is not a whitespace
+		if (s[i] !== " ") {
+			firstChar = true;
+			lenLastWord++;
+		}
+
+		// If the current character is a whitespace and we have already found the first non
+		// whitespace char
+		if (s[i] === " " && firstChar) {
+			break;
+		}
+	}
+
+	return lenLastWord;
+};
