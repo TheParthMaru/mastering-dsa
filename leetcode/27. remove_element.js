362ba49392b861dd786522816f341b8f02ac4cd0
Parth Maru <theparthmaru@gmail.com>
2024-01-31 20:44:20 +0530

    Added leetcode 27

---
+++ leetcode/27. remove_element.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ *
+ * Time complexity: O(N)
+ * Space complexity: O(1)
+ */
+var removeElement = function (arr, val) {
+	let i = 0;
+	let j = arr.length - 1;
+	let swaps = 0;
+
+	while (i <= j) {
+		if (arr[i] === val) {
+			[arr[i], arr[j]] = [arr[j], arr[i]];
+			swaps++;
+			j--;
+		} else {
+			i++;
+		}
+	}
+
+	let k = arr.length - swaps;
+	return k;
+};
