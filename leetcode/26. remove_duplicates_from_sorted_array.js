8fa691806c259fede6c53d4146e6de20a0297e52
Parth Maru <theparthmaru@gmail.com>
2024-02-03 16:43:03 +0530

    Added leetcode 26

---
+++ leetcode/26. remove_duplicates_from_sorted_array.js
@@ -0,0 +1,21 @@
+/**
+ * Tags: Arrays, Two pointers
+ * Difficulty: Easy
+ *
+ * Approach: Two pointer
+ * Time complexity: O(N)
+ * Space complexity: O(1)
+ */
+
+function solution(nums) {
+	let currUnique = 0;
+
+	for (let i = 1; i < nums.length; i++) {
+		if (nums[currUnique] != nums[i]) {
+			nums[currUnique + 1] = nums[i];
+			currUnique++;
+		}
+	}
+
+	return currUnique + 1;
+}
