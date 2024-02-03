a2bcefb5d532bbaffd6e52b38120949fd298b417
Parth Maru <theparthmaru@gmail.com>
2024-02-03 16:31:50 +0530

    added bruteforce approach

--- leetcode/14. longest_common_prefix.js
+++ leetcode/14. longest_common_prefix.js
@@ -2,6 +2,39 @@
  * Tags: String, Trie
  * Difficulty: Easy
  *
+ * Approach: Bruteforce
+ * Time complexity: O(N^2)
+ * Space complexity: O(1)
+ */
+
+function solution(strs) {
+	// Storing the first element for comparing with rest of the elements
+	let prefix = strs[0];
+	let j = 0;
+
+	// Starting with 1 because first element is already stored in prefix
+	for (let i = 1; i < strs.length; i++) {
+		let currWord = strs[i];
+		while (
+			j < prefix.length &&
+			j < currWord.length &&
+			prefix[j] === currWord[j]
+		) {
+			j++;
+		}
+
+		prefix = prefix.substring(0, j);
+
+		if (prefix == "") {
+			break;
+		}
+	}
+
+	return prefix;
+}
+
+/**
+ * Approach: Sorting the input array
  * Time complexity: O(nlogn + m)
  * Space complexity: O(N)
  */
