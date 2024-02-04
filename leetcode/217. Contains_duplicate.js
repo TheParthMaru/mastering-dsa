d16bc9bf41cf830d096983f8c45051243e9f96a8
Parth Maru <theparthmaru@gmail.com>
2024-02-04 09:58:42 +0530

    Added new approach

--- leetcode/217. Contains_duplicate.js
+++ leetcode/217. Contains_duplicate.js
@@ -1,16 +1,32 @@
-"use strict";
-
 /**
  * Problem tags: Arrays, hash table, sorting
  * Difficulty: Easy
  *
- * Approach 1: Creating a new Set from nums
+ * Approach: Creating a new Set from nums
  * Time complexity: O(n) (Because creating set from nums array of size n)
  * Space complexity: O(n)
  */
 
 function solution(nums) {
 	let mySet = new Set(nums);
-	if (nums.length === mySet.size) return false;
-	return true;
+	return !(mySet.size === nums.length);
+}
+
+/**
+ * Approach: Using sorting
+ * Time complexity: O(nlogn)
+ * Space complexity: O(1)
+ */
+
+function solution1(nums) {
+	nums.sort((x, y) => {
+		return x - y;
+	});
+
+	for (let i = 0; i < nums.length - 1; i++) {
+		if (nums[i] === nums[i + 1]) {
+			return true;
+		}
+	}
+	return false;
 }
