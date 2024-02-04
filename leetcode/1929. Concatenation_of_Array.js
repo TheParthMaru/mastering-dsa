34b9a644b6a8db84d3ef83c23104dd5b0db44797
Parth Maru <theparthmaru@gmail.com>
2024-02-04 10:21:13 +0530

    Added new approaches

--- leetcode/1929. Concatenation_of_Array.js
+++ leetcode/1929. Concatenation_of_Array.js
@@ -1,10 +1,8 @@
-"use strict";
-
 /**
  * Tags: Arrays, Simulation
  * Difficulty: Easy
  *
- * Approach 1:
+ * Approach: Mathematical approach using modulus
  * Time complexity: O(N)
  * Space complexity: O(N)
  */
@@ -15,4 +13,34 @@ function solution(nums) {
 		ans.push(nums[i % nums.length]);
 	}
 	return ans;
+}
+
+/**
+ * Approach: Bruteforce
+ * Time complexity: O(N)
+ * Space complexity: O(N)
+ */
+
+function solution2(nums) {
+	let ans = [];
+	let k = 0;
+	for (let i = 0; i < nums.length * 2; i++) {
+		if (k === nums.length) {
+			k = 0;
+		} else {
+			ans.push(nums[k]);
+			k++;
+		}
+	}
+	return ans;
+}
+
+/**
+ * Approach: Using built-in concat()
+ * Time complexity: O(N+M)
+ * Space complexity: O(N+M)
+ */
+
+function solution3(nums) {
+	return nums.concat(nums);
 }
