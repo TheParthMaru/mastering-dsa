de8fe6038abf3f4525a686a147028c6c5362bbac
Parth Maru <theparthmaru@gmail.com>
2024-02-16 21:02:45 +0530

    Added bubble sort code

---
+++ sortingAlgorithms/bubbleSort.js
@@ -0,0 +1,13 @@
+function bubbleSort(arr) {
+	for (let i = 0; i < arr.length - 1; i++) {
+		for (let j = 0; j < arr.length - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+			}
+		}
+	}
+}
+
+let arr = [5, 4, 3, 2, 1];
+bubbleSort(arr);
+console.log(arr);
