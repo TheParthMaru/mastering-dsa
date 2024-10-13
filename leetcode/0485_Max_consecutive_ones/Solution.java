1f46ed69b7b03e4bd199a637cb5e0e2d16eca1e1
Parth Maru <theparthmaru@gmail.com>
2024-10-13 07:49:32 +0100

    Added leetcode 485 Java solution

---
+++ leetcode/0485_Max_consecutive_ones/Solution.java
@@ -0,0 +1,24 @@
+/**
+ * Problem: Leetcode 485 -> Max consective ones
+ * Tags: Arrays
+ * Difficulty: Easy
+ * Time complexity: O(N)
+ * Space complexity: O(1)
+ */
+
+public class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int counter = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                ++counter;
+                max = Math.max(max, counter);
+            } else {
+                counter = 0;
+            }
+        }
+        return max;
+    }
+}
