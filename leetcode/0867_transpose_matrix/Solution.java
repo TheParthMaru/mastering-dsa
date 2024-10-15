7dc3920efc971183066939422a2fd6badf05b24f
Parth Maru <theparthmaru@gmail.com>
2024-10-15 10:23:50 +0100

    Added Java and JS solution for leetcode 867

---
+++ leetcode/0867_transpose_matrix/Solution.java
@@ -0,0 +1,25 @@
+/**
+ * Problem: Leetcode 867 -> Transpose matrix
+ * Tags: Array, Matrix, Simulation
+ * Difficulty: Easy
+ * 
+ * Time complexity: O(m x n)
+ * Space complexity: O(m x n)
+ */
+
+public class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int totalRows = matrix.length;
+        int totalCols = matrix[0].length;
+
+        int[][] result = new int[totalCols][totalRows];
+
+        for(int row = 0; row < totalRows; row++) {
+            for(int col = 0; col < totalCols; col++) {
+                result[col][row] = matrix[row][col];
+            }
+        }
+
+        return result;
+    }
+}
