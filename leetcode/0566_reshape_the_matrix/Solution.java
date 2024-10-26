0e069cb3572b3267e22e18d92e08ed1c66e9b26b
Parth Maru <theparthmaru@gmail.com>
2024-10-26 18:23:12 +0100

    Added notes and solution of leetcode problem 566

---
+++ leetcode/0566_reshape_the_matrix/Solution.java
@@ -0,0 +1,38 @@
+/**
+ * Leetcode: Problem 566 -> Reshape the matrix
+ * Topic: Array, Matrix, Simulation
+ * Difficulty: Easy
+ * 
+ * Time complexity: O(mxn)
+ * Space complexity: O(1)
+ */
+
+public class Solution {
+  public int[][] matrixReshape(int[][] mat, int r, int c) {
+    int m = mat.length;
+    int n = mat[0].length;
+
+    // When the data cannot be reshaped, simply return the orignal matrix
+    if (m * n != r * c) {
+      return mat;
+    }
+
+    int[][] result = new int[r][c];
+    int row = 0;
+    int col = 0;
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        result[row][col] = mat[i][j];
+        col++;
+
+        if (col == c) {
+          ++row;
+          col = 0;
+        }
+      }
+    }
+
+    return result;
+  }
+}
