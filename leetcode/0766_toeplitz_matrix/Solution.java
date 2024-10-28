b1cf694918a9f65f0ac6b6af85612aac652d976e
Parth Maru <theparthmaru@gmail.com>
2024-10-28 05:25:39 +0000

    Added notes and solution for leetcode problem 766

---
+++ leetcode/0766_toeplitz_matrix/Solution.java
@@ -0,0 +1,28 @@
+/**
+ * Problem: Leetcode 766 -> Toeplitz matrix
+ * Topics: Array, Matrix
+ * Difficulty: Easy
+ * 
+ * Time complexity: O(m x n)
+ * Space complexity: O(1)
+ */
+public class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        if (m * n == 1) {
+            return true;
+        }
+
+        for (int row = 1; row < m; row++) {
+            for (int col = 1; col < n; col++) {
+                if (matrix[row][col] != matrix[row - 1][col - 1]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
