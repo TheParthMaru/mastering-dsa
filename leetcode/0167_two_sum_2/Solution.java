d1e96c7aaed961c0bd6875240bfa0c5b865da865
Parth Maru <theparthmaru@gmail.com>
2024-11-21 21:13:24 +0000

    Added notes and solution of leetcode 167

---
+++ leetcode/0167_two_sum_2/Solution.java
@@ -0,0 +1,32 @@
+/**
+ * Problem: Leetcode 167 -> Two sum 2, input array is sorted
+ * Topics: Arrays, Two pointers, Binary search
+ * Difficulty: Medium
+ * 
+ * Time complexity: O(N)
+ * Space complexity: O(1)
+ */
+
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] result = new int[2];
+
+        int i = 0;
+        int j = numbers.length - 1;
+
+        while (i < j) {
+            int sum = numbers[i] + numbers[j];
+            if (sum == target) {
+                result[0] = i + 1;
+                result[1] = j + 1;
+                return result;
+            } else if (sum > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+
+        return result;
+    }
+}
