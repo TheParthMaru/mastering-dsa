c752b375089579441de869ec1159fa045af1996e
Parth Maru <theparthmaru@gmail.com>
2025-05-01 20:35:00 +0100

    Added recursive code for factorial

---
+++ 04_recursion/recursion_solution/Factorial.java
@@ -0,0 +1,8 @@
+public class Factorial {
+    public static int findFactorial(int num) {
+        if (num == 1 || num == 0) {
+            return 1;
+        }
+        return num * findFactorial(num - 1);
+    }
+}
