f40c61f1c3cff6658a8668fdb779fc7aaa82ae0b
Parth Maru <theparthmaru@gmail.com>
2025-05-01 20:58:06 +0100

    Added recursive code for printing natural numbers in decreasing order

---
+++ 04_recursion/recursion_solution/DecreasingNatNos.java
@@ -0,0 +1,10 @@
+public class DecreasingNatNos {
+    public static void decreasingNaturalNumbers(int num) {
+        System.out.print(num + " ");
+        if (num == 1) {
+            return;
+        }
+
+        decreasingNaturalNumbers(num - 1);
+    }
+}
