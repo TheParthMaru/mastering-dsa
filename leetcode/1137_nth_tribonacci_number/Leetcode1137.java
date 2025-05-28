86b0f00d375d8130f2a57688f1eba103f7d4a0db
Parth Maru <theparthmaru@gmail.com>
2025-05-28 19:34:49 +0100

    Added java code for leetcode 1137

--- leetcode/1137_nth_tribonacci_number/Leetcode1137.java
+++ leetcode/1137_nth_tribonacci_number/Leetcode1137.java
@@ -1,17 +1,19 @@
 public class Leetcode1137 {
 
     /*
-     * Approach 1: Straightforward recursion
+     * Approach 1: Straightforward recursion (results in TLE)
      * The problem with this approach is that it is very inefficient.
      * We could improve it with dynamic programming approach
      */
-    
-    public static int tribonacci(int n) {
+
+    public static int tribonacci1(int n) {
         // Base cases
-        if(n == 0) return 0;
+        if (n == 0)
+            return 0;
 
-        if(n == 1 || n == 2) return 1;
+        if (n == 1 || n == 2)
+            return 1;
 
-        return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
+        return tribonacci1(n - 1) + tribonacci1(n - 2) + tribonacci1(n - 3);
     }
 }
